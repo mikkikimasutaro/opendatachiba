51b3cbfcdaaaad0cb6e888ef69bb381331e6840b
Kensuke Kanamori <Kensuke.Kanamori@gmail.com>
2022-08-08 10:38:45 +0900

    modify

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,7 +10,7 @@ function Main() {
         <div style={{ width: '100%' }}>
             <h2>千葉県の人口・労働・経済等のデータを見やすくまとめ</h2>
             <div >
-             <img src="0009_r.png" alt="Logo" />;
+             <img src="0009_r.png" alt="Logo" />
              <MenuItem />
                 <Typography component="p" >
                         千葉県が公開するオープンデータを閲覧できるページです。データは <a href="https://www.pref.chiba.lg.jp/gyoukaku/opendata/index.html" target="_blank" rel="noopener noreferrer">千葉県オープンデータサイト</a>　から取得しています。
--- src/containers/PrivacyPolicy.js
+++ src/containers/PrivacyPolicy.js
@@ -17,7 +17,7 @@ function PrivacyPolicy() {
             お問い合わせに・ご意見ついては、以下のフォームよりお願いいたします。
 お問い合わせいただいた内容に含まれる個人情報につきましては、法律に違反する場合や公序良俗に反する場合を除き、第三者への提供はいたしません。
             </Typography>
-            <iframe title="inquiry" src="https://docs.google.com/forms/d/e/1FAIpQLScNSeiK9F0WviGrf-8h_UPHDy3yE6pWMJT0Az1sMX2s-gKutw/viewform?embedded=true" width="640" height="677" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+            <iframe title="inquiry" src="https://docs.google.com/forms/d/e/1FAIpQLScNSeiK9F0WviGrf-8h_UPHDy3yE6pWMJT0Az1sMX2s-gKutw/viewform?embedded=true" width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
 
         </div>
     );
